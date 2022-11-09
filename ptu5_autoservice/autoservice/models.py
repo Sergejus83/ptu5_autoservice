90dee95ce62b3b745ce511d7e92740e77139a9ab
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-09 09:32:04 +0200

    pridetas kainu skaiciavimas, sulpeliu pavadimai

--- ptu5_autoservice/autoservice/models.py
+++ ptu5_autoservice/autoservice/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _ # galima palikti su "_" - nebus klaidos
+from datetime import date
 
 # Create your models here.
 class CarModel(models.Model):
+    YEARS_CHOICES = ((years, str(years)) for years in reversed(range(1900, date.today().year+1))) 
+    # tai nera kintamasis (didziosem raidems) - tai funkcija, papildoma eilute nesusikuria
+
     make = models.CharField(_("make"), max_length=50)
     model = models.CharField(_("model"), max_length=50)
-    year = models.IntegerField(_("year"))
+    year = models.IntegerField(_("year"), choices = YEARS_CHOICES)
     engine = models.CharField(_("engine"), max_length=50)
 
     def __str__(self) -> str:
@@ -35,6 +39,18 @@ class Order(models.Model):
     total = models.DecimalField(_("total"), max_digits=20, decimal_places=2, default=0)
     date = models.DateField(_("date"), auto_now_add=True)
 
+    def get_total(self):
+        total = 0
+        for line in self.order_lines.all():
+            total += line.total
+            return total
+
+    def save(self,*args, **kwargs):
+        if not self._state.adding:
+            self.total = self.get_total()
+            super().save(*args, **kwargs)
+
+
     def __str__(self) -> str:
         return f"{self.date}: {self.total}"
 
@@ -44,6 +60,10 @@ class OrderLine(models.Model):
     service = models.ForeignKey(Service, verbose_name=_("service"), on_delete=models.CASCADE, related_name='order_lines',)
     quantity = models.IntegerField(_("quantity"), default=1)
     price = models.DecimalField(_("price"), max_digits=20, decimal_places=2)
+
+    @property
+    def total(self):
+        return self.quantity * self.price
 
     def __str__(self) -> str:
         return f"{self.service.name}: {self.quantity} x {self.price}"
