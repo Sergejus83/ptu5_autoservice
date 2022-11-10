9a50efc16c2f9bb967da1a37bdaaa9147bffe797
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-11 00:37:20 +0200

    sutvarkitas settings ir base.css. padarytas linkas

--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import CarModel, Car, Service, Order, OrderLine
 
 # Create your views here.
+
+def index(request):
+    # return HttpResponse("Testing")
+    name_count = Service.objects.count()
+    order_count = Order.objects.count()
+    car_count = Car.objects.count()
+
+    context = {
+        'name_count': name_count,
+        'car_count': car_count,
+        'order_count': order_count
+    }
+
+    return render(request, 'autoservice/index.html', context=context)
