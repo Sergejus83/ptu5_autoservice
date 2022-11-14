3a84a90c7287f4029093b18da8bc6bd1611a6e6b
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-14 08:53:31 +0200

    pages

--- ptu5_autoservice/autoservice/urls.py
+++ ptu5_autoservice/autoservice/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cars/', views.cars, name='cars'),
+    path('car/<int:car_id>/', views.car, name='car'),
+    path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('order/<int:pk>', views.OrderDetailView.as_view(), name='order'),
 ]
--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import CarModel, Car, Service, Order, OrderLine
+from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -17,3 +19,31 @@ def index(request):
     }
 
     return render(request, 'autoservice/index.html', context=context)
+
+def cars(request):
+    paginator = Paginator(Order.objects.all(), 2)
+    page_number = request.GET.get('page')
+    paged_cars = paginator.get_page(page_number)
+    return render(request, 'autoservice/cars.html', {'cars': paged_cars}) #{'cars': Car.objects.all()}) #
+
+def car(request, car_id):
+    return render(request, 'autoservice/car.html', {'car':get_object_or_404(Car, id = car_id)})
+
+class OrderListView(ListView):
+    model = Order
+    paginate_by = 1
+    template_name ='autoservice/orders_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['order_count'] = self.get_queryset().count()
+        return context
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name ='autoservice/order_detail.html'
+
+
+
+
+
